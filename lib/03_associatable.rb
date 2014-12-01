b12e3858d7ec035a96170e324334e5779eecee35
= <zachabrahams@gmail.com>
2014-12-01 17:52:06 -0500

    Adds classes BelongsToOptions, HasManyOptions to provide defaults for associations.

--- lib/03_associatable.rb
+++ lib/03_associatable.rb
@@ -6,27 +6,54 @@ class AssocOptions
   attr_accessor(
     :foreign_key,
     :class_name,
-    :primary_key
+    :primary_key,
+    :table_name
   )
 
+
   def model_class
-    # ...
+    class_name.constantize
   end
 
   def table_name
-    # ...
+    Module.const_get(class_name).table_name || class_name.tableize
   end
 end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    # ...
+    defaults = {
+      class_name: name.camelcase,
+      foreign_key: :"#{name}_id",
+      primary_key: :id
+    }
+
+    options = defaults.merge(options)
+
+
+    @foreign_key = options[:foreign_key]
+    @class_name = options[:class_name]
+    @primary_key = options[:primary_key]
+
+    self
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    # ...
+    defaults = {
+      class_name: name.camelcase.singularize,
+      foreign_key: :"#{self_class_name.underscore}_id",
+      primary_key: :id
+    }
+
+    options = defaults.merge(options)
+
+    @foreign_key = options[:foreign_key]
+    @class_name = options[:class_name]
+    @primary_key = options[:primary_key]
+
+    self
   end
 end
 
@@ -46,5 +73,5 @@ module Associatable
 end
 
 class SQLObject
-  # Mixin Associatable here...
+  extend Associatable
 end
