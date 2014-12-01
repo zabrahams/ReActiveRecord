06172ba8a41c748392fbabe30c57762bc87d8088
Zach Abrahams <zachabrahams@gmail.com>
2014-11-30 21:49:28 -0500

    Adds the following class methods to SQLObject: columns, finalize(bang), parse_all, all and find_by(id). Also adds the following instance methods: attribute_values, insert.

--- lib/01_sql_object.rb
+++ lib/01_sql_object.rb
@@ -20,11 +20,11 @@ class SQLObject
     cols.each do |column|
 
       define_method("#{column}") do
-        @attributes[column]
+        attributes[column]
       end
 
       define_method("#{column}=") do |value|
-        @attributes[column] = value
+        attributes[column] = value
       end
     end
   end
@@ -39,31 +39,73 @@ class SQLObject
   end
 
   def self.all
-    # ...
+    table = self.table_name
+    results = DBConnection.execute(<<-SQL)
+      SELECT
+        #{table}.*
+      FROM
+        #{table}
+    SQL
+    self.parse_all(results)
   end
 
   def self.parse_all(results)
-    # ...
+    results.map { |result| self.new(result) }
   end
 
   def self.find(id)
-    # ...
+    table = self.table_name
+    result = DBConnection.instance.get_first_row(<<-SQL, id)
+      SELECT
+        #{table}.*
+      FROM
+        #{table}
+      WHERE
+        #{table}.id = ?
+    SQL
+
+    result || (return nil)
+    self.new(result)
   end
 
   def initialize(params = {})
-    # ...
+    cols = self.class.columns
+    params.each do |attr_name, value|
+      attr_sym = attr_name.to_sym
+      unless cols.include?(attr_sym)
+        fail "unknown attribute '#{attr_name}'"
+      end
+      self.send(:"#{attr_name}=", value)
+    end
+
+    self
   end
 
   def attributes
-    @attributes = {}
+    @attributes || @attributes = {}
   end
 
   def attribute_values
-    # ...
+    cols = self.class.columns
+    cols.map { |column| self.send(column) }
   end
 
   def insert
-    # ...
+    table = self.class.table_name
+    cols = self.class.columns
+    cols.delete(:id)
+    non_id_attributes = attribute_values[1..-1]
+    DBConnection.execute(<<-SQL, *non_id_attributes)
+      INSERT INTO
+        #{table} (#{cols.join(", ")})
+      VALUES
+        (#{(["?"] * cols.count).join(", ")})
+    SQL
+
+    new_id = DBConnection.last_insert_row_id
+    self.attributes[:id] = new_id
+
+    self
   end
 
   def update
