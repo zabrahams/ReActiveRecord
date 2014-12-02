27cf6ec52786007af0c0067238969614722b1769
Zach Abrahams <zachabrahams@gmail.com>
2014-12-02 12:21:15 -0500

    Adds Relation#initialize, Relation#stack, and Relation#execute.  Also refactors Searchable::where to use the new Relation class.

--- lib/02_searchable.rb
+++ lib/02_searchable.rb
@@ -5,18 +5,13 @@ module Searchable
   def where(params)
     where_line = params.keys.map { |key| "#{key}= ?"}.join(" AND ")
     values = params.values
-    table = table_name
 
-    results = DBConnection.execute(<<-SQL, *values)
-      SELECT
-        *
-      FROM
-        #{table}
-      WHERE
-        #{where_line}
-    SQL
+    if self.class == Relation
+      self.stack(where_line, values)
+    else
+      Relation.new(self, table_name, where_line, values)
+    end
 
-    results.map { |result| self.new(result) }
   end
 end
 
@@ -25,4 +20,57 @@ class SQLObject
 end
 
 class Relation
+  include Searchable
+
+  def initialize(target_class, table_name, where_line, values)
+    @target_class = target_class
+    @table_name = table_name
+    @where_line = where_line
+    @values = values
+  end
+
+  def stack(new_where_line, new_values)
+    @where_line = "(#{@where_line}) AND (#{new_where_line})"
+    @values = @values + new_values
+    self
+  end
+
+  def execute
+    results = DBConnection.execute(<<-SQL, *@values)
+    SELECT
+    *
+    FROM
+    #{@table_name}
+    WHERE
+    #{@where_line}
+    SQL
+
+    @target_class.parse_all(results)
+  end
+
+end
+
+module OldSearchable
+  def where(params)
+    where_line = params.keys.map { |key| "#{key}= ?"}.join(" AND ")
+    values = params.values
+    table = table_name
+
+    results = DBConnection.execute(<<-SQL, *values)
+    SELECT
+    *
+    FROM
+    #{table}
+    WHERE
+    #{where_line}
+    SQL
+
+    self.parse_all(results)
+  end
+end
+
+## Testing code below!
+
+class Cat < SQLObject
+  self.finalize!
 end
